9fc66bd18d093bf40674d9e9f5c99663685394de
SerbanPrecup <precup.serban@yahoo.com>
2025-02-18 13:03:41 +0200

    lab1 ex3 refacere

--- lab1/ex3.py
+++ lab1/ex3.py
@@ -5,10 +5,14 @@ E = ['0','1','2']
 def is_pal(s):
     return s == s[::-1]
 
-def generate_pal(alf, max_length):
+def generate_pal(alfabet, max_length):
     for length in range(1, max_length + 1):
         print(f"Palindroame de lungime {length}:")
-        palindromes = ["".join(p) for p in product(alf, repeat=length) if is_pal("".join(p))]
+        palindromes = []
+        for p in product(alfabet, repeat=length):
+            word = "".join(p)  # trasf in sir
+            if is_pal(word):
+                palindromes.append(word)
         print(" ".join(palindromes), "\n")
 
 generate_pal(E, 5)
